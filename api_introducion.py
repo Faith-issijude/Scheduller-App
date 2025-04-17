56f4825bd371b20012d99bbd44dff859bdf9dfc3
Issijude Faithfulness <89425939+Faith-issijude@users.noreply.github.com>
2025-04-17 11:16:56 +0100

    Add files via upload

---
+++ api_introducion.py
@@ -0,0 +1,12 @@
+#Application Programming Interfaces (APIs)
+#An Api is a set of rules and specifications that allow different software systems to communicate and exchange data
+#between each other.
+#why use api
+#1. Increased Functionality: enable you to integrate external services and data into your application, expanding their capabilities
+#2. Time and cost savings: you can leverage existing API services, saving development time and resources
+#3. Enhanced User Experience: APIs can provide seamless integration with other applications, making it easier for users 
+#to access and interact with your application.
+#ANATOMY OF AN API
+#1. Endpoints: these are the URLs that you use to interact with the API
+#2. Requests: Data sent to to the api to request info or perform actions
+#3. Responses: data sent back from the api
